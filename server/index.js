e04ac247de45567f6d0a4cd711fd489ef0dc3acb
stefano demichelis and Matthew Cook <steppashr+mc255v@gmail.com>
2019-07-24 15:53:54 +0900

    finished basic requirements

--- server/index.js
+++ server/index.js
@@ -9,20 +9,200 @@ const schema = buildSchema(`
   type Pokemon {
     id: String
     name: String!
+    classification: String
+    types: [String]
+    resistant: [String]
+    weaknesses: [String]
+    weight: WeightHeight
+    height: WeightHeight
+    fleeRate: Float
+    evolutionRequirements: EvoReq
+    evolutions: [Evol]
+    maxCP: Int
+    maxHP: Int
+    attacks: Attacks
   }
+  type Attacks {
+    fast: [Attack]
+    special: [Attack]
+  }
+  type Attack {
+    name: String
+    type: String
+    damage: Int
+  }
+
+  type Evol {
+    id: Int
+    name: String
+  }
+  type EvoReq {
+    amount: Int
+    name: String
+  }
+  type WeightHeight {
+    minimum: String
+    maximum: String
+  }
+
   type Query {
     Pokemons: [Pokemon]
-    Pokemon(name: String!): Pokemon
+    PokeType(type: String!): [Pokemon]
+    Pokettack(name: String!): [Pokemon]
+    Pokemon(name: String, id: String): Pokemon
+    Attacks(type: String!): [Attack]
+    Types: [String]
+  }
+
+  type Mutation {
+    addPokemon(input: PokeInput): Pokemon
+    editPokemon(name: String!, input: PokeInput): Pokemon
+    deletePokemon(name: String!): Pokemon
+    addType(input: String!): [String]
+    editType(name: String!, input: String!): [String]
+    deleteType(name: String!): [String]
+    addAttack(type: String!, input: Atk) : [Attack]
+    editAttack(name: String!, input: Atk): Attack
+    deleteAttack(input: String!): Attack
+  }
+  
+  input PokeInput {
+    id: String
+    name: String!
+    classification: String
+    types: [String]
+    resistant: [String]
+    weaknesses: [String]
+    weight: WH
+    height: WH
+    fleeRate: Float
+    evolutionRequirements: EvoR
+    evolutions: [Evo]
+    maxCP: Int
+    maxHP: Int
+    attacks: Atks
+  }
+  input WH {
+    minimum: String
+    maximum: String
+  }
+  input EvoR {
+    amount: Int
+    name: String
+  }
+  input Evo {
+    id: Int
+    name: String
+  }
+  input Atks {
+    fast: [Atk]
+    special: [Atk]
+  }
+  input Atk {
+    name: String
+    type: String
+    damage: Int
   }
 `);
 
 // The root provides the resolver functions for each type of query or mutation.
 const root = {
+  addPokemon: (input) => {
+    data.pokemon.push(input.input);
+    return input.input;
+  },
+  editPokemon: (input) => {
+    const caught = data.pokemon.find((pokemon) => pokemon.name === input.name);
+    for (const key in input.input) {
+      caught[key] = input.input[key];
+    }
+    return caught;
+  },
+  deletePokemon: (input) => {
+    const caught = data.pokemon.find((pokemon) => pokemon.name === input.name);
+    const index = data.pokemon.indexOf(caught);
+    data.pokemon.splice(index, 1);
+    return caught;
+  },
   Pokemons: () => {
     return data.pokemon;
   },
   Pokemon: (request) => {
-    return data.pokemon.find((pokemon) => pokemon.name === request.name);
+    const nameOrId = request.name ? "name" : "id";
+    return data.pokemon.find(
+      (pokemon) => pokemon[nameOrId] === request[nameOrId]
+    );
+  },
+  Attacks: (request) => {
+    return data.attacks[request.type];
+  },
+  addAttack: (input) => {
+    data.attacks[input.type].push(input.input);
+    return data.attacks[input.type];
+  },
+  editAttack: (input) => {
+    const allAttacks = data.attacks.fast.concat(data.attacks.special);
+    for (const key of allAttacks) {
+      if (key.name === input.name) {
+        Object.assign(key, input.input);
+      }
+    }
+    return input.input;
+  },
+
+  deleteAttack: (input) => {
+    const attack = data.attacks.fast
+      .concat(data.attacks.special)
+      .find((attack) => {
+        return attack.name === input.input;
+      });
+    if (data.attacks.fast.includes(attack)) {
+      const index = data.attacks.fast.indexOf(attack);
+      data.attacks.fast.splice(index, 1);
+    } else {
+      const index = data.attacks.special.indexOf(attack);
+      data.attacks.special.splice(index, 1);
+    }
+    return attack;
+  },
+
+  Types: () => {
+    return data.types;
+  },
+  addType: (input) => {
+    data.types.push(input.input);
+    return data.types;
+  },
+  editType: (input) => {
+    const index = data.types.indexOf(input.name);
+    data.types.splice(index, 1, input.input);
+    return data.types;
+  },
+  deleteType: (input) => {
+    const index = data.types.indexOf(input.name);
+    data.types.splice(index, 1);
+    return data.types;
+  },
+  PokeType: (request) => {
+    return data.pokemon.filter((pokemon) => {
+      for (const type of pokemon.types) {
+        if (type === request.type) {
+          return true;
+        }
+        return false;
+      }
+    });
+  },
+  Pokettack: (request) => {
+    return data.pokemon.filter((pokemon) => {
+      const allAttacks = pokemon.attacks.fast.concat(pokemon.attacks.special);
+      for (const attack of allAttacks) {
+        if (attack.name === request.name) {
+          return true;
+        }
+      }
+      return false;
+    });
   },
 };
 
